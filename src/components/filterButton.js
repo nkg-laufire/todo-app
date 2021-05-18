401918d46f37fe5c30f4e7c35c1782be25598874
sriram000 <srirammani14@gmail.com>
2021-05-18 15:14:38 +0530

    Hid the filter buttons when there are no todos.

--- src/components/filterButton.js
+++ src/components/filterButton.js
@@ -1,9 +1,16 @@
 import { React } from 'react';
 import context from '../core/context';
+import TodoManager from '../services/todoManager';
 
-const FilterButton = (filter) =>
-	<button onClick={ () => context.actions.setFilter(filter) }>
-		{ filter }
-	</button>;
+const FilterButton = (filter) => {
+	const { todos } = context.state;
+	const noTodos = TodoManager.getTodoCount(todos) === 0;
+
+	return noTodos
+		? null
+		:	<button onClick={ () => context.actions.setFilter(filter) }>
+			{ filter }
+		</button>;
+};
 
 export default FilterButton;
