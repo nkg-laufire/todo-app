73e85b0393314273e28981012a31adf9afe463e5
sriram000 <srirammani14@gmail.com>
2021-05-18 16:44:55 +0530

    Added actionKeys to the input box.

--- src/components/todoInput.js
+++ src/components/todoInput.js
@@ -1,10 +1,16 @@
 import { React } from 'react';
 import context from '../core/context';
 
+const actionKeys = {
+	Enter: () => context.actions.addTodo(),
+	Escape: () => context.actions.updateInput(''),
+};
+
 const TodoInput = () =>
 	<input
 		value={context.state.input}
-		onChange={(evt) => context.actions.updateInput(evt.target.value)}
+		onChange={ (evt) => context.actions.updateInput(evt.target.value) }
+		onKeyUp={ (evt) => actionKeys[evt.code] && actionKeys[evt.code]() }
 	/>;
 
 export default TodoInput;
