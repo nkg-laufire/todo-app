02facc5fc56af4f073b62d944811e9af4969ce67
sriram000 <srirammani14@gmail.com>
2021-05-20 17:45:26 +0530

    Refactored.

--- src/services/taskRetriver.js
+++ src/services/taskRetriver.js
@@ -1,3 +1,4 @@
+import { range } from '@laufire/utils/collection';
 import { rndBetween } from '@laufire/utils/random';
 import faker from 'faker';
 import config from '../core/config';
@@ -7,15 +8,8 @@ const toProperCase = (txt) => txt.charAt(0).toUpperCase() + txt.substr(1);
 const getRndTask = () =>
 	toProperCase(`${ faker.hacker.verb() } ${ faker.hacker.noun() }.`);
 
-const getTasks = () => {
-	const count = rndBetween(0, config.maxTaskCount);
-	const tasks = [];
-
-	for(let i = 0; i < count; i++)
-		tasks.push(getRndTask());
-
-	return tasks;
-};
+const getTasks = () =>
+	range(1, rndBetween(0, config.maxTaskCount)).map(getRndTask);
 
 const TaskRetriever = {
 	getTasks,
