4c921a7e0d7190d2d94a44bfe265e6edfc4bd651
sriram000 <srirammani14@gmail.com>
2021-05-26 12:45:58 +0530

    Highlighted active filter button.

--- src/components/todoPane/filterButton.js
+++ src/components/todoPane/filterButton.js
@@ -1,15 +1,26 @@
 import { React } from 'react';
 import context from '../../core/context';
 import TodoManager from '../../services/todoManager';
+import { equals } from '@laufire/utils/collection';
+
+const activeStyle = {
+	background: 'lightgrey',
+};
+
+const inactiveStyle = {};
 
 // eslint-disable-next-line react/prop-types
 const FilterButton = ({ key, filter }) => {
 	const { todos } = context.state;
 	const noTodos = TodoManager.count(todos) === 0;
 
+	const activeFilter = context.state.filter;
+	const isActive = equals(filter, activeFilter);
+	const style = isActive ? activeStyle : inactiveStyle;
+
 	return noTodos
 		? null
-		:	<button key={ key }
+		:	<button style={ style } key={ key }
 			onClick={ () => context.actions.setFilter(filter) }>
 			{ key }
 		</button>;
